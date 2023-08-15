948155324f7ed2fd83f9c5560ada4faa4e01f316
BigHacker123 <116747999+BigHacker123@users.noreply.github.com>
2023-08-15 05:41:08 +0100

    Create .lua

---
+++ .lua
@@ -0,0 +1,15 @@
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
+
+local games = {
+    [{13680982680}] = "https://raw.githubusercontent.com/BigHacker123/velocitycc2/main/strengthsimulator.lua",
+    -- Games
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        Notification.Notify("velocity.cc", "Loading "..GameName,"rbxassetid://4483345998");
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
